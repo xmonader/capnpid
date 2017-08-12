86b442fcbca190aad995195b1e59d5b9ab845878
Ahmed T. Youssef <xmonader@gmail.com>
2017-08-12 06:43:26 +0200

    fixes for min capnp id generated. check if it's less than 1<<63, version bump

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import * as crypto from "crypto";
-
-
-/*
 import * as bignum from "big-integer";
-*/
+
 export function randInt(min = 0, max: any) {
     return min + Math.floor(Math.random() * (max - min + 1));
 }
 export function calcCapnpID() {
-    // return bignum(randInt(0, Math.pow(2, 64))).or(bignum(1).shiftLeft(63)).toString(16);
-    return crypto.randomBytes(8).toString("hex");
+    const num = crypto.randomBytes(8).toString("hex");
+    return bignum(num, 16).or(bignum(1).shiftLeft(63)).toString(16);
+
 }
 export function newCapnpID() {
     return `@0x${calcCapnpID()};`;
